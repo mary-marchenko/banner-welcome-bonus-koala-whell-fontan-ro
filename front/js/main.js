cec242fbfb96b4382d4488e7018ac0609942b094
Mari <mari@MacBook-Pro.local>
2024-10-25 13:47:06 +0300

    ipad12 fix + andr/wind fix

--- front/js/main.js
+++ front/js/main.js
@@ -6,3 +6,24 @@
     setHeight();
     window.addEventListener('resize', setHeight);
 })();
+
+window.addEventListener("orientationchange", () => {
+    location.reload(true)
+});
+
+// color padding-top for banner__title-top
+document.addEventListener("DOMContentLoaded", function() {
+    function isIOS() {
+        return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+    }
+
+    function isMacOS() {
+        return navigator.platform === 'MacIntel' && !navigator.userAgent.includes('Mobile');
+    }
+
+    let bannerTitleTop = document.querySelector('.banner__txt');
+
+    if (!isIOS() && !isMacOS() && bannerTitleTop) {
+        bannerTitleTop.classList.add('mar-pad');
+    }
+});
